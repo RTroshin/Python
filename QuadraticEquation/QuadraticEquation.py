a21058082fdb61a6e6153ac315bb4fac17deb0bd
RTroshin <R.R.Troshin@outlook.com>
2020-10-07 23:00:13 +0300

    Program QuadraticEquation
    Update file QuadraticEquation.py

--- QuadraticEquation/QuadraticEquation.py
+++ QuadraticEquation/QuadraticEquation.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 
 print('\nДано квадратное уравнение вида ax^2 + bx + c = 0')
 
@@ -21,10 +21,10 @@ else:
 
 	if D >= 0:
 		if D == 0:
-			x = (-b + math.sqrt(D)) / 2 * a
+			x = (-b + sqrt(D)) / 2 * a
 			print('{}{:.2f}{}'.format('\nДва равных корня:\nx1,2 = ', x, '\n'))
 		else:
-			sqD = math.sqrt(D)
+			sqD =sqrt(D)
 			x1 = (-b + sqD) / 2 * a
 			x2 = (-b - sqD) / 2 * a
 			print('{}{}{:.2f}{}{:.2f}{}'.format('\nКорень x1 и x2:', '\nx1 = ', x1, '  x2 = ', x2, '\n'))

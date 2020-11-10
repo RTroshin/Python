7e41d5eef6a701acb6a500e94885c1388809a1ce
RTroshin <R.R.Troshin@outlook.com>
2020-11-10 23:40:36 +0300

    Program EnglishDictionary
    New file EnglishDictionary.py

---
+++ EnglishDictionary/EnglishDictionary.py
@@ -0,0 +1,96 @@
+print()
+print('Вас приветствует программа "Словарь"')
+englishWords = ['cat', 'apple', 'car', 'water']
+russianWords = ['кошка', 'яблоко', 'автомобиль', 'вода']
+
+while True:
+    print('Меню\n')
+    print('1. Просмотреть словарь')
+    print('2. Добавить слово в словарь')
+    print('3. Редактировать слово')
+    print('4. Удалить слово из словаря\n')
+    userChoice = input('Выберите пункт меню: ')
+
+    # Вывод словаря на экран
+
+    if (userChoice == '1'):
+        if len(englishWords) == 0:
+            print('Ваш словарь пуст!\n')
+        else:
+            number = 1
+            print('-' * 43)
+            print('|  №  |      Слово      |     Перевод     |')
+            print('-' * 43)
+            for i in range(len(englishWords)):
+                print('|{:^5d}| {:<15} | {:<15} |'\
+                      .format(number, englishWords[i], russianWords[i]))
+                print('-' * 43)
+                number += 1
+        print()
+
+    # Добавление слова в словарь
+
+    if (userChoice == '2'):
+        add = True
+        print('Добавление слова в словарь')
+        enWord = input('Введите слово на английском языке: ')
+        ruWord = input('Введите перевод слова на русском языке: ')
+        print('-' * 43)
+        for i in range(len(englishWords)):
+            if enWord.lower() == englishWords[i]:
+                print('\nТакое слово уже есть в Вашем словаре!')
+                add = False
+                break
+        if add == True:
+            englishWords.append(enWord.lower())
+            russianWords.append(ruWord.lower())
+            print('\nСлово "', enWord, '" добавлено!')
+        print()
+
+    # Редактировать слово
+
+    if (userChoice == '3'):
+        if len(englishWords) == 0:
+            print('Ваш словарь пуст!\n')
+        else:
+            cor = True
+            
+            listNumber = int(input('Редактирование слова\n\
+Выберите номер слова из списка: '))
+            print('-' * 43)
+            while (listNumber <= 0) or (listNumber > len(englishWords)):
+                print('Номер в списке не найден!\nПопробуйте ещё раз!\n')
+                listNumber = int(input('Выберите номер слова из списка: '))
+            OldEnWord = englishWords[listNumber - 1]
+            OldRoWord = russianWords[listNumber - 1]
+            NewEnWord = input('\nРедактируйте слово: ')
+            NewRuWord = input('Редактируйте перевод слова: ')
+            for i in range(len(englishWords)):
+                if NewEnWord.lower() == englishWords[i]:
+                    print('\nТакое слово уже есть в Вашем словаре!')
+                    cor = False
+                    break
+            if cor == True:
+                englishWords.remove(englishWords[listNumber - 1])
+                russianWords.remove(russianWords[listNumber - 1])
+                englishWords.insert(listNumber - 1, NewEnWord.lower())
+                russianWords.insert(listNumber - 1, NewRuWord.lower())
+                print('\nСлово ', OldEnWord, ' (', OldRoWord,')\
+ отредактировано на ', NewEnWord, ' (', NewRuWord,')')
+        print()
+
+    # Удаление слова из словаря
+
+    if (userChoice == '4'):
+        if len(englishWords) == 0:
+            print('Ваш словарь пуст!\n')
+        else:
+            listNumber = int(input('Удаление слова из словаря\n\
+Выберите номер слова из списка: '))
+            print('-' * 43)
+            while (listNumber <= 0) or listNumber > len(englishWords):
+                print('Номер в списке не найден!\nПопробуйте ещё раз!\n')
+                listNumber = int(input('Выберите номер слова из списка: '))
+            englishWords.remove(englishWords[listNumber - 1])
+            russianWords.remove(russianWords[listNumber - 1])
+        print()
